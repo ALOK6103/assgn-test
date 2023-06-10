ba3a78bcecc4dc6ae04326d88514117fb5991da5
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-06-10 22:26:56 +0530

    added backend

--- routes/seat.routes.js
+++ routes/seat.routes.js
@@ -29,31 +29,55 @@ seatRouter.post("/reserve",async(req,res)=>{
 
   try {
     const numSeats = req.body.numSeats;
-
+    
     // Find available seats in one row
-    const availableSeats = await seatModel.find({isBooked: false })
+    const availableSeatsInOneRow = await seatModel.find({
+      isBooked: false,
+      seatNumber: { $lt: 8 - numSeats },
+    })
       .sort({ row: 1, seatNumber: 1 })
       .limit(numSeats);
 
-    if (availableSeats.length < numSeats || numSeats>7) {
-      return res.status(400).json({ error: 'Not enough available seats' });
+    if (availableSeatsInOneRow.length === numSeats) {
+      // Reserve seats in one row
+      availableSeatsInOneRow.forEach(async (seat) => {
+        seat.isBooked = true;
+        await seat.save();
+      });
+      return res.json({ message: 'Seats reserved successfully' });
+    } else {
+      // Reserve seats in nearby rows
+      const reservedSeats = await seatModel.find({ isBooked: true }).sort({ row: -1, seatNumber: -1 }).limit(1);
+      const lastReservedSeatRow = reservedSeats.length > 0 ? reservedSeats[0].row : 0;
+
+      const availableSeatsNearby = await seatModel.find({
+        isBooked: false,
+        row: { $gt: lastReservedSeatRow },
+      })
+        .sort({ row: 1, seatNumber: 1 })
+        .limit(numSeats);
+
+      if (availableSeatsNearby.length < numSeats) {
+        return res.status(400).json({ error: 'Not enough available seats' });
+      }
+
+      // Reserve seats in nearby rows
+      availableSeatsNearby.forEach(async (seat) => {
+        seat.isBooked = true;
+        await seat.save();
+      });
+
+      return res.json({ message: 'Seats reserved successfully' });
     }
-
-    // Reserve the seats
-    availableSeats.forEach(async (seat) => {
-      seat.isBooked = true;
-      await seat.save();
-    });
-
-    res.send({ message: 'Seats booked successfully' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ error: 'server error' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 })
 
 module.exports={
   seatRouter
 }
+
 
 
